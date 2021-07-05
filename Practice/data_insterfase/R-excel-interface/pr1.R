45501b5eb539ea2dbf07bc93c5d270d16e1a5ab3
jaffarabbas <gamaportal8@gmail.com>
2021-07-05 21:55:45 +0500

    7-5-2021

--- Practice/data_insterfase/R-excel-interface/pr1.R
+++ Practice/data_insterfase/R-excel-interface/pr1.R
@@ -5,5 +5,5 @@ any(grepl("xlsx",installed.packages()))
 
 library("xlsx")
 # Read the first worksheet in the file input.xlsx.
-data <- read.xlsx("../SampleData.xlsx", sheetIndex = 2)
+data <- read.xlsx("../excel_file/SampleData.xlsx", sheetIndex = 2)
 print(data)
