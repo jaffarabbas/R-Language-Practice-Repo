56a6a8f41e7f0c5b03070e57c5845dc29aec1e53
jaffarabbas <gamaportal8@gmail.com>
2021-07-05 21:55:56 +0500

    7-5-2021

---
+++ Practice/loops/pr1.R
@@ -0,0 +1,24 @@
+x = 1:5
+
+print(x)
+
+#for
+
+for(i in x){
+    if(i%%2==0){
+        print("Integer")
+    }else{
+        print(i)
+    }
+}
+
+#while
+
+p = 0
+while(p<=10){
+    if(p%%2){
+        next   
+    }
+    p=p+1
+    print(p)
+}
