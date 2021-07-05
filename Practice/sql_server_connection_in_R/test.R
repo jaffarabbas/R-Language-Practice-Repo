fc84cf0b4ba7ff149cef9cc1909c478fad025b7d
jaffarabbas <gamaportal8@gmail.com>
2021-07-05 21:56:03 +0500

    7-5-2021

--- Practice/sql_server_connection_in_R/test.R
+++ Practice/sql_server_connection_in_R/test.R
@@ -6,13 +6,24 @@ library(ggplot2)
 # Check what odbc drivers are installed and available
 unique(odbcListDrivers()[[1]])
 
-con <- odbc::dbConnect(odbc(), Driver = "SQL Server", 
-                       Server = "DESKTOP-FFPMVV9\\DARKLORD", 
-                       Database = "Scott", 
-                       uid = "jaffar", 
-                       pwd="jaffar")
+con <- odbc::dbConnect(odbc(), Driver = "SQL Server", Server = "DESKTOP-FFPMVV9\\DARKLORD", Database = "Scott", uid = "jaffar", pwd="jaffar")
+count_employ <- DBI::dbGetQuery(con, "Select top 5 salary from EMPLOYEE")
+count_employ_name <- DBI::dbGetQuery(con, "Select top 5 name from EMPLOYEE")
 
-d <- DBI::dbGetQuery(con,"Select * from EMPLOYEE")
-df <- DBI::dbReadTable(con, 'EMPLOYEE')
+print(count_employ)
 
-print(d)
+
+# Create data for the graph.
+x <- c(count_employ)
+labels <- c(count_employ_name)
+
+# Give the chart file a name.
+png(file = "subjectMarks444.png")
+
+# Plot the chart.
+pie(x, labels)
+
+print(count_employ)
+
+# Save the file.
+dev.off()
