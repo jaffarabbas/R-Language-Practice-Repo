bba9c3a07c18a487b74a50b7cf8dab61b36e3866
jaffarabbas <gamaportal8@gmail.com>
2021-07-06 20:43:48 +0500

    7-6-2021

---
+++ Practice/functions/pr1.R
@@ -0,0 +1,34 @@
+first <- function(){
+    print("hellllllllllllllll")
+}
+
+first()
+
+powerfunc <- function(num,power){
+    return (num^power)
+}
+
+powerfunc(2,3)
+
+squarefunc <- function(num){return(num1*num2)}
+squarefunc(4)
+
+#paste
+
+add <- function(num,num2){
+    sum <- num+num2
+    print(sum)
+}
+
+print(paste(add(4,4)))
+
+#factorial
+
+
+factorialFunction <- function(fact,num){
+    for(i in num){
+        fact = fact*i
+    }
+    print(num)
+}
+factorialFunction(5,1)
